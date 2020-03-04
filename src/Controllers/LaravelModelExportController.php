e82de05c268caa7e0846f710386225bc94bb8619
Damilola Olowookere <damms005@gmail.com>
2020-03-05 00:46:52 +0100

    save dowloaded files with a more unique name

--- src/Controllers/LaravelModelExportController.php
+++ src/Controllers/LaravelModelExportController.php
@@ -9,6 +9,13 @@ use Rap2hpoutre\FastExcel\FastExcel;
 
 class LaravelModelExportController extends Controller
 {
+	/**
+	 * The cache of snake-cased words.
+	 *
+	 * @var array
+	 */
+	protected static $snakeCache = [];
+
 	public function index()
 	{
 		$configuredModels = collect(config('laravel-model-export.exportableModels'));
@@ -32,16 +39,56 @@ class LaravelModelExportController extends Controller
 	public function download(Request $request)
 	{
 		$filterProperty = $request->input('filterProperty');
-		$model          = $request->input('model');
-		$items          = (new $model())
-			->where($filterProperty, '>=', $request->input('filterLower'))
-			->where($filterProperty, '<=', $request->input('filterUpper'))
+
+		$model = $request->input('model');
+
+		$items = (new $model())
+			->when($request->filled('filterLower'), function ($query) use ($filterProperty, $request) {
+				$query
+					->where($filterProperty, '>=', $request->input('filterLower'))
+					->where($filterProperty, '<=', $request->input('filterUpper'));
+			})
 			->get();
 
 		if ($items->isEmpty()) {
 			return "No items in the selected range";
 		} else {
-			return (new FastExcel($items))->configureCsv('\t', '', '\n', "UTF-8")->download('file.xlsx');
+			return (new FastExcel($items))->configureCsv('\t', '', '\n', "UTF-8")->download("{$this->snake($model)}-" . \date(DATE_ATOM) . ".xlsx");
 		}
+	}
+
+	/**
+	 * Convert a string to snake case.
+	 *
+	 * @param  string  $value
+	 * @param  string  $delimiter
+	 * @return string
+	 */
+	public static function snake($value, $delimiter = '_')
+	{
+		$key = $value;
+
+		if (isset(static::$snakeCache[$key][$delimiter])) {
+			return static::$snakeCache[$key][$delimiter];
+		}
+
+		if (!ctype_lower($value)) {
+			$value = preg_replace('/\s+/u', '', ucwords($value));
+
+			$value = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1' . $delimiter, $value));
+		}
+
+		return static::$snakeCache[$key][$delimiter] = $value;
+	}
+
+	/**
+	 * Convert the given string to lower-case.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function lower($value)
+	{
+		return mb_strtolower($value, 'UTF-8');
 	}
 }
