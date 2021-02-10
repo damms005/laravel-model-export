e62e798f75794801eb31c778826867d78c725a05
Damilola Olowookere <damms005@gmail.com>
2021-02-10 12:22:51 +0100

    Improve README

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # laravel-model-export
 
-Easily export Eloquent models in Laravel, GUI ready!
+[![Latest Stable Version](https://poser.pugx.org/damms005/laravel-model-export/v)](//packagist.org/packages/damms005/laravel-model-export) [![Total Downloads](https://poser.pugx.org/damms005/laravel-model-export/downloads)](//packagist.org/packages/damms005/laravel-model-export) [![License](https://poser.pugx.org/damms005/laravel-model-export/license)](//packagist.org/packages/damms005/laravel-model-export)
 
-Spoiler alert: a little ranting :)
+![Art image for laravel-model-export](https://banners.beyondco.de/laravel-model-export.png?theme=light&packageManager=composer+require&packageName=damms005%2Flaravel-model-export&pattern=circuitBoard&style=style_1&description=Easily+filter+and+export+Eloquent+models+in+Laravel%2C+GUI+ready&md=1&showWatermark=1&fontSize=100px&images=tag&widths=350)
+
+Easily export Eloquent models in Laravel, GUI ready!
 
 This Laravel package is born out of my love for, and everyday use of [Voyager](https://github.com/the-control-group/voyager). There are times that I need to move/copy some of my Voyager-managed Eloquent data for use on some other third-party platform/share with client. Sometimes too I give client access to download their data in Excel. Voyager does not provide this export feature currently. Therefore, this package may become obsolete the very day we can filter and download eloquent models directly from Voyager.
 
-Needless to say, **this package is not Voyager-dependent nor is it tightly coupled with Voyager**. It simply a Laravel package that provides the ability to export Eloquent models based on your filter options.
+Needless to say, **this package is not Voyager-dependent nor is it tightly coupled with Voyager**. It simply a Laravel package that provides the ability to export Eloquent models. You can also filter the data before exporting.
 
 # Installation
+
 `composer require damms005/laravel-model-export`
 
 # Usage
-After installation, the `laravel-model-export` url is registered to your routes. So visit: `http://yourwebsite.com/laravel-model-export`
+
+After installation, the `/laravel-model-export` url is registered to your routes. So visit: `http://<yourwebsite.com>/laravel-model-export`
 
 # Todo:
 
--- views/index.blade.php
+++ views/index.blade.php
@@ -6,36 +6,45 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Laravel Model Export</title>
-	<link rel="stylesheet" href="{{asset('vendor/laravel-model-export.php/css/tailwind.css')}}">
-	<link rel="stylesheet" href="{{asset('vendor/laravel-model-export.php/css/tailwind-v1.css')}}">
-	<script type="text/javascript" src="{{asset('vendor/laravel-model-export.php/js/vue.js')}}"></script>
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js"></script>
 </head>
 
 <body>
 	<form method="POST" action="{{route('laravel-model-export.download')}}">
 		{{ csrf_field() }}
 		<div id="laravel-model-export" class="p-24 hover:bg-grey-lightest">
-			<h2 class="font-bold mt-4">Model</h2>
-			<select class="w-64 p-2 border rounded" name="model" v-model="selectedModel">
-				<option selected></option>
-				<option v-for=" modelEntry , modelName  in exportableModels " :value="modelName">${modelName}</option>
-			</select>
+			<div class="font-bold text-2xl ">
+				Easily export Models from your database
+			</div>
 
-			<h2 class="font-bold mt-4">Filter Property:</h2>
-			<select class="w-64 p-2 border rounded" name="filterProperty" v-model="modelFilterProperty">
-				<option selected></option>
-				<option v-for=" property in ( displayedModel ? Object.values( displayedModel ) : {} ) " :value="property">${property}</option>
-			</select>
+			<div class='my-10'>
+				<h2 class="font-bold mt-4">Model</h2>
+				<select class="w-64 p-2 border rounded" name="model" v-model="selectedModel">
+					<option selected></option>
+					<option v-for=" modelEntry , modelName  in exportableModels " :value="modelName">${modelName}</option>
+				</select>
+			</div>
 
-			<h2 class="font-bold mt-4">Lower Filter Range</h2>
-			<input class="w-64 p-2 border rounded" type="text" name="filterLower" id="">
+			<div class='my-10'>
+				<h2 class="font-bold mt-4">Filter Property:</h2>
+				<select class="w-64 p-2 border rounded" name="filterProperty" v-model="modelFilterProperty">
+					<option selected></option>
+					<option v-for=" property in ( displayedModel ? Object.values( displayedModel ) : {} ) " :value="property">${property}</option>
+				</select>
+			</div>
 
-			<h2 class="font-bold mt-4">Upper filter range</h2>
-			<input class="w-64 p-2 border rounded" type="text" name="filterUpper" id="">
+			<div class='my-10'>
+				<h2 class="font-bold mt-4">Lower Filter Range <span v-html="filteredPropertyHelper"></span></h2>
+				<input class="w-64 p-2 border rounded" type="text" name="filterLower" id="">
+			</div>
 
-			<br>
-			<br>
-			<input class="rounded p-2 border bg-blue" type="submit" value="Submit">
+			<div class='my-10'>
+				<h2 class="font-bold mt-4">Upper filter range <span v-html="filteredPropertyHelper"></span></h2>
+				<input class="w-64 p-2 border rounded" type="text" name="filterUpper" id="">
+			</div>
+
+			<input class="rounded p-2 border bg-blue-400 px-5 my-5" type="submit" value="Submit">
 		</div>
 	</form>
 
@@ -54,6 +63,12 @@
 				displayedModel: function(){
 					return this.exportableModels[this.selectedModel];
 				},
+
+				filteredPropertyHelper: function(){
+					if(this.modelFilterProperty){
+						return `for <span class="italic text-gray-600">${this.modelFilterProperty}</span>`;
+					}
+				}
 
 			},
 
